208a3d612d67c9ab4a62a875cdaf581afd5a320c
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-03-19 17:11:13 -0600

    Prueba ram

--- Nodejs/controller/ram.js
+++ Nodejs/controller/ram.js
@@ -2,18 +2,18 @@ const { Response, Request } = require('express');
 const fs = require('fs'); 
 
 const get_pol = (req, res = Response) => {
-    const data_ram = () => (fs.readFileSync('/elements/procs/ram-module', 'utf8')).toString();
+    const data_ram = fs.readFileSync('/elements/procs/ram-module', 'utf8').toString();
 
     res.send({
-        'data': data_ram()
+        'data': data_ram
     });
 }
 
 const get_util = (req, res = Response) => {
-    const data_ram = () => (fs.readFileSync('/elements/procs/ram-module', 'utf8')).toString();
+    const data_ram = fs.readFileSync('/elements/procs/ram-module', 'utf8').toString();
 
     res.send({
-        'uso': data_ram(),
+        'uso': data_ram,
         'libre': '200'
     });
 }
