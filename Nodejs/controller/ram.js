ec1b0f6ea3e245c12ec28353f6730124d0368e33
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-03-19 20:10:46 -0600

    Add dockerfiles

--- Nodejs/controller/ram.js
+++ Nodejs/controller/ram.js
@@ -6,25 +6,28 @@ const get_pol = (req, res = Response) => {
     const data_mod = JSON.parse(data_ram);
 
     //Y = Data en enteros %
-    //X = Data en 
+    let porc_ocupado = (data_ram.ocupado / data_ram.total) * 100;
     res.send({
-        'y': data_mod.ocupado,
+        'y': porc_ocupado,
         'label': ''
     });
 }
 
 const get_util = (req, res = Response) => {
-    const data_ram = fs.readFileSync('/procs/ram-module', 'utf8').toString();
+    const data_ram = fs.readFileSync('/elements/procs/ram-module', 'utf8').toString();
 
     //Data mandar la cantidad libre 
+    let espacio_libre = data_ram.total - data_ram.ocupado;
+    let porc_libre = (espacio_libre / data_ram.total) * 100;
+    let porc_ocupado = (data_ram.ocupado / data_ram.total) * 100;
     res.send(
         [
             {
-                'y': data_ram,
+                'y': porc_libre,
                 'label': 'libre'
             },
             {
-                'y': data_ram,
+                'y': porc_ocupado,
                 'label': 'ocupado'
             }
         ]
