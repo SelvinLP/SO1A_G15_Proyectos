9fdc1c8b9e91fcc94b4ea73d39fc76bef376d502
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-03-19 20:47:29 -0600

    Terminado ram

--- Nodejs/controller/ram.js
+++ Nodejs/controller/ram.js
@@ -6,7 +6,7 @@ const get_pol = (req, res = Response) => {
     const data_mod = JSON.parse(data_ram);
 
     //Y = Data en enteros %
-    let porc_ocupado = (data_ram.ocupado / data_ram.total) * 100;
+    let porc_ocupado = (data_mod.ocupado / data_mod.total) * 100;
     res.send({
         'y': porc_ocupado,
         'label': ''
@@ -15,11 +15,12 @@ const get_pol = (req, res = Response) => {
 
 const get_util = (req, res = Response) => {
     const data_ram = fs.readFileSync('/elements/procs/ram-module', 'utf8').toString();
+    const data_mod = JSON.parse(data_ram);
 
     //Data mandar la cantidad libre 
-    let espacio_libre = data_ram.total - data_ram.ocupado;
-    let porc_libre = (espacio_libre / data_ram.total) * 100;
-    let porc_ocupado = (data_ram.ocupado / data_ram.total) * 100;
+    let espacio_libre = data_mod.total - data_mod.ocupado;
+    let porc_libre = (espacio_libre / data_mod.total) * 100;
+    let porc_ocupado = (data_mod.ocupado / data_mod.total) * 100;
     res.send(
         [
             {
